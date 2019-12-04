5195490d1271ceb23dbe3a9949d9a4145a451cd9
NormanLiana <liananorman@pursuit.org>
2019-12-03 23:32:30 -0500

    creates FIR auth service to create, log in, sign out, and update a user

--- SwitchCraft/Utilities/Firebase/FirebaseAuthService.swift
+++ SwitchCraft/Utilities/Firebase/FirebaseAuthService.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class FirebaseAuthService {
+    
+    static let manager = FirebaseAuthService()
+    
+    private let auth = Auth.auth()
+    
+    var currentUser: User? {
+        return auth.currentUser
+    }
+    
+    func createNewUser(email: String, password: String, completion: @escaping (Result<User, Error>) -> () ) {
+        auth.createUser(withEmail: email, password: password) { (result, error) in
+            if let createdUser = result?.user {
+                completion(.success(createdUser))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func logInUser(email: String, password: String, completion: @escaping (Result<(), Error>) -> () ) {
+        auth.signIn(withEmail: email, password: password) { (result, error) in
+            if let _ = result?.user {
+                completion(.success(()))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func signOutUser() {
+        do {
+            try auth.signOut()
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func updateUserProfile(userName: String? = nil, photoURL: URL? = nil, completion: @escaping (Result<(), Error>) -> ()) {
+        let changeRequest = auth.currentUser?.createProfileChangeRequest()
+        if let userName = userName {
+            changeRequest?.displayName = userName
+        }
+        
+        if let photoURL = photoURL {
+            changeRequest?.photoURL = photoURL
+        }
+        
+        changeRequest?.commitChanges(completion: { (error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        })
+    }
+
+    
+    private init () {}
+}
