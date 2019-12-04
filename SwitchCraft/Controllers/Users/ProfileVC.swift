690982b87e53ddc7a3d32e7bf44d3d832692ae6c
NormanLiana <liananorman@pursuit.org>
2019-12-03 23:24:23 -0500

    adds 2 properties, one of type AppUser, one for current user

--- SwitchCraft/Controllers/Users/ProfileVC.swift
+++ SwitchCraft/Controllers/Users/ProfileVC.swift
@@ -10,21 +10,13 @@ import UIKit
 
 class ProfileVC: UIViewController {
 
+    // MARK: - Properties
+    var user: AppUser!
+    var isCurrentUser = false
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
