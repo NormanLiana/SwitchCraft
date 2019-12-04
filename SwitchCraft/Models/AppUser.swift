bbd04a1b7403211bd4983c946e88ee61cdf51888
NormanLiana <liananorman@pursuit.org>
2019-12-03 23:15:08 -0500

    builds model for app user to be stored in firestore

--- SwitchCraft/Models/AppUser.swift
+++ SwitchCraft/Models/AppUser.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+struct AppUser {
+    let email: String?
+    let photoURL: String?
+    let uid: String
+    let userName: String?
+    
+    
+    init(from user: User) {
+        self.email = user.email
+        self.uid = user.uid
+        self.photoURL = user.photoURL?.absoluteString
+        self.userName = user.displayName
+    }
+    
+    init?(from dict: [String:Any], id: String) {
+        guard let email = dict["email"] as? String,
+        let photoURL = dict["photoURL"] as? String,
+        let userName = dict["userName"] as? String else {return nil}
+        
+        self.photoURL = photoURL
+        self.email = email
+        self.uid = id
+        self.userName = userName
+        
+    }
+    
+    var fieldsDict: [String:Any] {
+        return [
+            "userName": self.userName ?? "",
+            "email": self.email ?? ""
+        ]
+    }
+}
