6f9bd6a5d107e2f405053dbcc9cb8da9ee824842
NormanLiana <liananorman@pursuit.org>
2019-12-04 00:06:51 -0500

    creates FIR service that creates and updates a user

--- SwitchCraft/Utilities/Firebase/FirestoreService.swift
+++ SwitchCraft/Utilities/Firebase/FirestoreService.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+fileprivate enum FireStoreCollections: String {
+    case users
+    case posts
+}
+
+enum SortingCriteria: String {
+    case fromNewestToOldest = "dateCreated"
+    var shouldSortDescending: Bool {
+        switch self {
+        case .fromNewestToOldest:
+            return true
+        }
+    }
+}
+
+class FirestoreService {
+    
+    static let manager = FirestoreService()
+    
+    private let db = Firestore.firestore()
+    
+    // MARK: - Creating and Updating Users
+    func createAppUser(user: AppUser, completion: @escaping (Result<(), Error>) -> ()) {
+        var fields: [String : Any] = user.fieldsDict
+        fields["dateCreated"] = Date()
+        db.collection(FireStoreCollections.users.rawValue).document(user.uid).setData(fields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+            }
+            completion(.success(()))
+        }
+    }
+    
+    func updateCurrentUser(userName: String? = nil, photoURL: URL? = nil, completion: @escaping (Result <(), Error>) -> ()) {
+        guard let userID = FirebaseAuthService.manager.currentUser?.uid else {
+            return
+        }
+        
+        var updateFields = [String : Any]()
+        if let user = userName {
+            updateFields["userName"] = user
+        }
+        
+        if let photo = photoURL {
+            updateFields["photoURL"] = photo.absoluteString
+        }
+        
+       // MARK: - The PUT Request
+        db.collection(FireStoreCollections.users.rawValue).document(userID).updateData(updateFields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
+    
+    
+    
+    private init() {}
+}
