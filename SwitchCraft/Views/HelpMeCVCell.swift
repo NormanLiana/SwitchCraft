8cef8d2717e0011eaa02ec21410427e3c0ea7821
NormanLiana <liananorman@pursuit.org>
2019-12-23 22:37:28 -0500

    adds cocoa file for CV cell to views folder

---
+++ SwitchCraft/Views/HelpMeCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  HelpMeCVCell.swift
+//  SwitchCraft
+//
+//  Created by Liana Norman on 12/23/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class HelpMeCVCell: UICollectionViewCell {
+    
+}
