692ae2581c46ecec742dfdd0d3946d2fbfc85c6e
NormanLiana <liananorman@pursuit.org>
2019-12-26 23:01:58 -0500

    adds UI objects

--- SwitchCraft/Controllers/Users/ProfileVC.swift
+++ SwitchCraft/Controllers/Users/ProfileVC.swift
@@ -9,6 +9,57 @@
 import UIKit
 
 class ProfileVC: UIViewController {
+    
+    // MARK: - UI Objects
+    lazy var profileImage: UIImageView = {
+        let img = UIImageView()
+        img.backgroundColor = .brown
+        return img
+    }()
+    
+    lazy var nameLabel: UILabel = {
+       let label = UILabel()
+       label.backgroundColor = .brown
+       return label
+    }()
+    
+    lazy var locationLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .brown
+        return label
+    }()
+    
+    lazy var bioTextView: UITextView = {
+        let tv = UITextView()
+        tv.backgroundColor = .brown
+        return tv
+    }()
+    
+    lazy var directMessageButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Send Message", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        return btn
+    }()
+    
+    lazy var serviceRequestButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Send Message", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        return btn
+    }()
+    
+    lazy var availableHoursLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .brown
+        return label
+    }()
+    
+    lazy var pendingHoursLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .brown
+        return label
+    }()
 
     // MARK: - Properties
     var user: AppUser!
