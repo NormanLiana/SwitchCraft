bacdc521eddeb530cf3200c8b78a55ef9a750081
NormanLiana <liananorman@pursuit.org>
2019-12-26 23:21:21 -0500

    adds all UI objects as subviews and adds constraints for profile img and name label

--- SwitchCraft/Controllers/Users/ProfileVC.swift
+++ SwitchCraft/Controllers/Users/ProfileVC.swift
@@ -68,6 +68,42 @@ class ProfileVC: UIViewController {
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubviews()
+        addConstraints()
+    }
+    
+    // MARK: - Private Methods
+    private func addSubviews() {
+        view.addSubview(profileImage)
+        view.addSubview(nameLabel)
+        view.addSubview(locationLabel)
+        view.addSubview(bioTextView)
+        view.addSubview(directMessageButton)
+        view.addSubview(serviceRequestButton)
+        view.addSubview(availableHoursLabel)
+        view.addSubview(pendingHoursLabel)
+    }
+    
+    private func addConstraints() {
+        constrainProfileIMG()
+        constrainNameLabel()
+    }
+    
+    private func setUpVCView() {
+        view.backgroundColor = .white
+    }
+    
+    // MARK: - Constraint Methods
+    private func constrainProfileIMG() {
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        [profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15), profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15), profileImage.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.37), profileImage.heightAnchor.constraint(equalTo: profileImage.widthAnchor)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainNameLabel() {
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        [nameLabel.bottomAnchor.constraint(equalTo: profileImage.centerYAnchor), nameLabel.leadingAnchor.constraint(equalTo: profileImage.trailingAnchor, constant: 15), nameLabel.widthAnchor.constraint(equalTo: profileImage.widthAnchor, multiplier: 1.4), nameLabel.heightAnchor.constraint(equalTo: profileImage.heightAnchor, multiplier: 0.3)].forEach({$0.isActive = true})
     }
     
 }
